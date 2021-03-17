abcf8f17ba0b354ddc3ca783edd231ddbdc29514
Boggi174 <57078913+Boggi174@users.noreply.github.com>
2021-03-17 17:15:54 -0600

    3D Model Upload

--- QuarantGymApp/app/src/main/java/com/example/quarantgymapp/MainActivity.java
+++ QuarantGymApp/app/src/main/java/com/example/quarantgymapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quarantgymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+        Uri intentUri =
+                Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+                        .appendQueryParameter("file", "")
+                        .appendQueryParameter("mode", "ar_preferred")
+                        .appendQueryParameter("title", "Untitled")
+                        .build();
+        sceneViewerIntent.setData(intentUri);
+        sceneViewerIntent.setPackage("com.google.ar.core");
+        startActivity(sceneViewerIntent);
     }
+
+
 }
